12a2415ddfa34269294fd3a0a9f754332d63ee5d
NLCaceres <NLCaceres21@Gmail.com>
2024-11-08 22:12:22 -0800

    Add Echo Logger middleware via `app.Use` for simpler debugging
    - Echo Middleware MUST be set BEFORE running `app.Start`

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	fmt.Println("Let's build with Go")
+	app := echo.New()
+	// `Use` must be used & declared BEFORE starting the app
+	app.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true, LogURI: true, LogError: true,
+		HandleError: true, // Forward errors to the global handler to decide status code
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			if v.Error == nil { // Println provides a simple way of concatening strings with vars with spaces injected between
+				fmt.Println("REQUEST URL =", v.URI, "&", "REQUEST Status =", v.Status)
+			} else { // Printf provides an old-school Python style of interpolating vars into a string
+				fmt.Printf("ERROR on REQUEST URL = %v with Status %v & Error = %v", v.URI, v.Status, v.Error)
+			}
+			return nil
+		},
+	}))
+	// app.Use(middleware.Logger()) // Original Echo Logger but not as extensible
+	app.Logger.Debug(app.Start("localhost:3000"))
 }
