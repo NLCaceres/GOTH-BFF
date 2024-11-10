c5297849c56149ca068887048f30ba483a6454ee
NLCaceres <NLCaceres21@Gmail.com>
2024-11-09 17:26:41 -0800

    Setup routes in its own func + via Env Vars
    - Routes currently return a simple "Hello" string with their readable
    route name. In the future, they'll have handlers properly mapped

---
+++ internal/route/route.go
@@ -0,0 +1,39 @@
+package route
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// NOTE: Public funcs in Go start with a capital 1st letter, no keyword needed
+func Routes(app *echo.Echo) {
+	routeMap := make(map[string]string)
+	// NOTE: Golang ISN'T functional so no `map`, `filter`, etc. Just need to use `for`
+	for _, routeKeyVal := range strings.Split(os.Getenv("ROUTE_MAP"), ",") {
+		// Gets each from ["route:routeReadable", "foo:Foo"]
+		splitKeyVal := strings.Split(routeKeyVal, ":") // Then ["route", "routeReadable"]
+		routePath, routeReadable := splitKeyVal[0], splitKeyVal[1]
+		routeMap[routePath] = routeReadable
+	}
+
+	appRoutes := strings.Split(os.Getenv("APP_ROUTES"), ",") // Load comma-separated routes from `.env`
+	for _, route := range appRoutes {
+		routePath := "/" + route
+
+		app.GET(routePath, func(c echo.Context) error {
+			var routeFormatted string
+			if routeReadable, ok := routeMap[route]; ok { // `ok` is false if no value exists for given key
+				routeFormatted = routeReadable // No formatting needed if readable version of route exists in map
+			} else {
+				routeFormatted = cases.Title(language.English).String(route)
+			}
+
+			return c.String(http.StatusOK, fmt.Sprintf("Hello %s", routeFormatted))
+		})
+	}
+}
