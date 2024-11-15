5e4fa34d54a994c15115fa1f44616d412e176b9b
NLCaceres <NLCaceres21@Gmail.com>
2024-11-15 00:11:08 -0800

    Create func to setup external POST requests
    - Accepts URL, contentType AND a body to be used by a `http.Client`
    setup in the function
    - Returns the JSON data received after being Unmarshaled in a simple to parse map
    - Any errors thrown are also returned to be handled by the func caller

---
+++ internal/util/external_api.go
@@ -0,0 +1,32 @@
+package util
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+)
+
+func PostRequest(url string, contentType string, body io.Reader) (map[string]interface{}, error) {
+	tr := &http.Transport{}
+	client := &http.Client{Transport: tr}
+	client.Timeout = time.Second * 15
+
+	var responseData map[string]interface{}
+
+	response, err := client.Post(url, contentType, body) // Sets the "Content-Type" header for you!
+
+	if err != nil {
+		return responseData, err
+	}
+
+	//INFO: Closing the response body after reading helps the client/server reuse their TCP connection
+	defer response.Body.Close()
+	responseBody, err := io.ReadAll(response.Body)
+
+	if err = json.Unmarshal(responseBody, &responseData); err != nil {
+		return responseData, err
+	}
+
+	return responseData, err
+}
