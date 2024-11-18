c662704ce3cdc0bd1720766c1ea8c28009280b22
NLCaceres <NLCaceres21@Gmail.com>
2024-11-18 00:12:37 -0800

    Convert the PostRequest func tests into a single table test
    - Since all 4 of the original tests followed a pattern of
    setting up the server, making the POST request, and checking its
    response and errors, the conversion was both simple and sensible
    to DRY up the test file

--- internal/util/external_api_test.go
+++ internal/util/external_api_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"bytes"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -36,72 +37,50 @@ func createHandlerFunc(t *testing.T, mock HttpMock) http.HandlerFunc {
 	})
 }
 
-func TestPostRequestError(t *testing.T) {
-	server := httptest.NewServer(createHandlerFunc(t, HttpMock{"POST", "/foo", 403, `{"foo":"bar"}`, map[string]string{}}))
-	defer server.Close()
-
-	// Can break the URL with an ASCII Ctrl Char (0-31 or 177). 0x7f is the 177 DEL Char
-	badChar := string([]byte{0x7f}) // Directly converting via string(0x7f) issues a warning
-	serverURL := server.URL + "/foo" + badChar
-	responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
-	// Still should get a map back, but it'll be empty (and therefore equal to nil)
-	if responseData != nil && len(responseData) != 0 {
-		t.Errorf("Response map is unexpectedly filled with length of %v", len(responseData))
+func TestPostRequest(t *testing.T) {
+	var tests = map[string]struct {
+		PostURL          string
+		ServerMock       HttpMock
+		ExpectedResponse map[string]interface{}
+		ExpectedErr      error
+	}{
+		"Error within POST itself": { // Using ASCII Ctrl Char (DEL aka 177) to break Server URL
+			"/foo" + string([]byte{0x7f}), HttpMock{"POST", "/foo", 403, `{"foo":"bar"}`, map[string]string{}},
+			nil, errors.New("parse net/url error"),
+		},
+		"Error Reading Response": { // An empty Response w/out a StatusCode BUT a Content-Length header of "1"
+			"/foo", HttpMock{"POST", "/foo", 0, ``, map[string]string{"Content-Length": "1"}},
+			nil, errors.New("unexpected EOF Error"), // Causes this EOF Error
+		},
+		"Error due to Malformed JSON Response": { // No brackets surrounding the JSON response causes this error
+			"/foo", HttpMock{"POST", "/foo", 202, `"foo":"bar"`, map[string]string{}},
+			nil, errors.New("invalid character at top-level of JSON Error"),
+		},
+		"Successfully POSTed": {
+			"/foo", HttpMock{"POST", "/foo", 202, `{"foo":"bar"}`, map[string]string{}},
+			map[string]interface{}{"foo": "bar"}, nil,
+		},
 	}
-	if err == nil {
-		t.Error("Error was nil, but should have been a parsing error from the `net/url` package")
-	}
-}
 
-func TestResponseReadError(t *testing.T) {
-	// Adding the "Content-Length" Header w/out setting any response data or status causes an EOF error
-	server := httptest.NewServer(createHandlerFunc(t, HttpMock{
-		"POST", "/foo", 0, ``, map[string]string{"Content-Length": "1"},
-	}))
-	defer server.Close()
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			server := httptest.NewServer(createHandlerFunc(t, test.ServerMock))
+			defer server.Close()
 
-	serverURL := server.URL + "/foo"
-	responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
+			serverURL := server.URL + test.PostURL
+			responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
 
-	if responseData != nil && len(responseData) != 0 {
-		t.Errorf("Response map is unexpectedly filled with length of %v", len(responseData))
-	}
-	if err == nil {
-		t.Errorf("Error was nil, but should have been an EOF error")
-	}
-}
-
-func TestJsonUnmarshalError(t *testing.T) {
-	// A badly formatted JSON string, like one without surrounding brackets, causes a JSON error
-	server := httptest.NewServer(createHandlerFunc(t, HttpMock{
-		"POST", "/foo", 202, `"foo":"bar"`, map[string]string{},
-	}))
-	defer server.Close()
-
-	serverURL := server.URL + "/foo"
-	responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
-
-	if responseData != nil && len(responseData) != 0 {
-		t.Errorf("Response map is unexpectedly filled with length of %v", len(responseData))
-	}
-	if err == nil {
-		t.Errorf("Error was nil, but should have been a JSON error")
-	}
-}
-
-func TestPostSuccess(t *testing.T) {
-	server := httptest.NewServer(createHandlerFunc(t, HttpMock{
-		"POST", "/foo", 202, `{"foo":"bar"}`, map[string]string{},
-	}))
-	defer server.Close()
-
-	serverURL := server.URL + "/foo"
-	responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
-
-	if err != nil {
-		t.Errorf("Error expected to be nil but got %v", err)
-	}
-	if value, ok := responseData["foo"]; responseData == nil || len(responseData) == 0 || !ok || value != "bar" {
-		t.Errorf("Response map expected to contain key-val pairs but got %v", responseData)
+			if test.ExpectedResponse == nil && responseData != nil {
+				t.Errorf("Response data expected to be nil but was actually filled")
+			}
+			for key, expectedValue := range test.ExpectedResponse {
+				if actualValue, ok := responseData[key]; !ok || expectedValue != actualValue {
+					t.Errorf("Response map key %v found value of %v instead of %v", key, actualValue, expectedValue)
+				}
+			}
+			if (test.ExpectedErr == nil && err != nil) || (test.ExpectedErr != nil && err == nil) {
+				t.Errorf("Error unexpectedly = %v when it shouldn't have been", err)
+			}
+		})
 	}
 }
