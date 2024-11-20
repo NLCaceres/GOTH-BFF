745d8887a3a01bfe94e6596380c66a092f42265e
NLCaceres <NLCaceres21@Gmail.com>
2024-11-20 00:04:56 -0800

    Use `util/test` Mocks & make a simple `newHttpMock` factory
    - Replaces the old package private Mocks with the `util/test` versions
    - The `newHttpMock` package private helper simply creates HttpMock
    instances with common defaults, only allowing the test to set the
    status code, data, and request headers

--- internal/util/external_api_test.go
+++ internal/util/external_api_test.go
@@ -3,84 +3,64 @@ package util
 import (
 	"bytes"
 	"errors"
-	"net/http"
+	"github.com/NLCaceres/goth-example/internal/util/test"
 	"net/http/httptest"
 	"testing"
 )
 
-type HttpMock struct { // Lowercased Struct fields ARE package private! Even if it'd be fine here
-	RequestMethod   string
-	RequestURL      string
-	ResponseStatus  int
-	ResponseData    string
-	ResponseHeaders map[string]string
-}
-
-func createHandlerFunc(t *testing.T, mock HttpMock) http.HandlerFunc {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		if r.Method != mock.RequestMethod {
-			t.Errorf("Unexpected Request method = %s", r.Method)
-		}
-		// Following could be useful BUT not really interested in checking the URL
-		// if r.URL.Path != mock.RequestURL {
-		// 	t.Errorf("Unexpected POST Request to URL = %s", r.URL.Path)
-		// }
-		if mock.ResponseStatus > 0 {
-			rw.WriteHeader(mock.ResponseStatus)
-		}
-		if mock.ResponseData != "" {
-			rw.Write([]byte(mock.ResponseData))
-		}
-		for key, value := range mock.ResponseHeaders {
-			rw.Header().Set(key, value)
-		}
-	})
-}
-
 func TestPostRequest(t *testing.T) {
 	var tests = map[string]struct {
 		PostURL          string
-		ServerMock       HttpMock
+		ServerMock       test.HttpMock
 		ExpectedResponse map[string]interface{}
 		ExpectedErr      error
 	}{
-		"Error within POST itself": { // Using ASCII Ctrl Char (DEL aka 177) to break Server URL
-			"/foo" + string([]byte{0x7f}), HttpMock{"POST", "/foo", 403, `{"foo":"bar"}`, map[string]string{}},
+		"Error within POST itself": { // Using ASCII Ctrl Char (DEL aka 177) breaks the Server URL
+			"/foo" + string([]byte{0x7f}), newHttpMock(403, `{"foo":"bar"}`, nil),
 			nil, errors.New("parse net/url error"),
 		},
-		"Error Reading Response": { // An empty Response w/out a StatusCode BUT a Content-Length header of "1"
-			"/foo", HttpMock{"POST", "/foo", 0, ``, map[string]string{"Content-Length": "1"}},
-			nil, errors.New("unexpected EOF Error"), // Causes this EOF Error
+		"Error Reading Response": { // An empty Response w/out a StatusCode
+			"/foo", newHttpMock(0, ``, map[string]string{"Content-Length": "1"}), // BUT a Content-Length header of "1"
+			nil, errors.New("unexpected EOF Error"), // causes this EOF Error
 		},
-		"Error due to Malformed JSON Response": { // No brackets surrounding the JSON response causes this error
-			"/foo", HttpMock{"POST", "/foo", 202, `"foo":"bar"`, map[string]string{}},
-			nil, errors.New("invalid character at top-level of JSON Error"),
+		"Error due to Malformed JSON Response": {
+			"/foo", newHttpMock(202, `"foo":"bar"`, nil), // No brackets surrounding JSON response
+			nil, errors.New("invalid character at top-level of JSON Error"), // Causes this error
 		},
 		"Successfully POSTed": {
-			"/foo", HttpMock{"POST", "/foo", 202, `{"foo":"bar"}`, map[string]string{}},
+			"/foo", newHttpMock(202, `{"foo":"bar"}`, nil),
 			map[string]interface{}{"foo": "bar"}, nil,
 		},
 	}
 
-	for testName, test := range tests {
+	for testName, testCase := range tests {
 		t.Run(testName, func(t *testing.T) {
-			server := httptest.NewServer(createHandlerFunc(t, test.ServerMock))
+			server := httptest.NewServer(test.NewTestHandlerFunc(t, testCase.ServerMock))
 			defer server.Close()
 
-			serverURL := server.URL + test.PostURL
+			serverURL := server.URL + testCase.PostURL
 			responseData, err := PostRequest(serverURL, "application/json", bytes.NewBuffer([]byte(`{"foo":"bar"}`)))
 
-			if test.ExpectedResponse == nil && responseData != nil {
+			if testCase.ExpectedResponse == nil && responseData != nil {
 				t.Errorf("Response data expected to be nil but was actually filled")
 			}
-			for key, expectedValue := range test.ExpectedResponse {
+			for key, expectedValue := range testCase.ExpectedResponse {
 				if actualValue, ok := responseData[key]; !ok || expectedValue != actualValue {
 					t.Errorf("Response map key %v found value of %v instead of %v", key, actualValue, expectedValue)
 				}
 			}
-			if (test.ExpectedErr == nil && err != nil) || (test.ExpectedErr != nil && err == nil) {
+			if (testCase.ExpectedErr == nil && err != nil) || (testCase.ExpectedErr != nil && err == nil) {
 				t.Errorf("Error unexpectedly = %v when it shouldn't have been", err)
 			}
 		})
+	}
+}
+
+// Package private helper to condense HttpMock initialization
+// Particularly the headers, which when set to `nil` seemingly equal `map[string]string{}`
+func newHttpMock(statusCode int, data string, headers map[string]string) test.HttpMock {
+	return test.HttpMock{ //NOTE: Structs from other packages REQUIRED field names to protect from breaking changes
+		RequestMethod: "POST", RequestURL: "/foo",
+		ResponseStatus: statusCode, ResponseData: data, ResponseHeaders: headers,
 	}
 }
