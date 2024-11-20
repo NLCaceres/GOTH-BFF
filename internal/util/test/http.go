4297379736ee2d14b94705554392a3184a1c89c6
NLCaceres <NLCaceres21@Gmail.com>
2024-11-19 16:46:50 -0800

    Split HttpMock and Server Route Handler Mock Func into reusable units
    - Since both the HttpMock struct and `NewTestHandlerFunc` can be
    reused by other `httptest` related test files, it makes sense to include
    it in a `test` directory in the `util` directory, reusable across the
    entire app and its tests

---
+++ internal/util/test/http.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"net/http"
+	"testing"
+)
+
+type HttpMock struct { // Lowercased Struct fields ARE package private!
+	RequestMethod   string
+	RequestURL      string
+	ResponseStatus  int
+	ResponseData    string
+	ResponseHeaders map[string]string
+}
+
+func NewTestHandlerFunc(t *testing.T, mock HttpMock) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		if r.Method != mock.RequestMethod {
+			t.Errorf("Unexpected Request method = %s", r.Method)
+		}
+		// Following could be useful BUT not really interested in checking the URL
+		// if r.URL.Path != mock.RequestURL {
+		// 	t.Errorf("Unexpected POST Request to URL = %s", r.URL.Path)
+		// }
+		if mock.ResponseStatus > 0 {
+			rw.WriteHeader(mock.ResponseStatus)
+		}
+		if mock.ResponseData != "" {
+			rw.Write([]byte(mock.ResponseData))
+		}
+		for key, value := range mock.ResponseHeaders {
+			rw.Header().Set(key, value)
+		}
+	})
+}
