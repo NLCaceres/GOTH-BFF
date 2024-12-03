06b4f2a24641a481ec28b60572b7bec4f0f62605
NLCaceres <NLCaceres21@Gmail.com>
2024-12-03 10:49:32 -0800

    Add helpers to grab Project file paths by name
    - Provides a global variable that always links back to the
    Project root folder path
    - Provides a helper func to output a valid path to a given
    project file
    - Straightforward tests included to be absolutely sure that
    the correct paths are output

---
+++ internal/util/project_path.go
@@ -0,0 +1,27 @@
+package util
+
+import (
+	"path/filepath"
+	"runtime"
+)
+
+// Path of the Project Root Folder
+var ProjectRoot = findProjectRoot()
+
+// Uses runtime.Caller to get this file's name
+// Then uses the file's name to get its directory
+// And jump back from `util` to `internal` then the project root
+// To be reusable, it expects future Go projects to follow a similar folder structure
+func findProjectRoot() string {
+	_, b, _, _ := runtime.Caller(0)
+	projectRoot := filepath.Join(filepath.Dir(b), "../..")
+
+	return projectRoot
+}
+
+// Takes the path of a file relative to the project root folder
+// and returns the concatenated path as a string ready for use
+// by funcs like `Open` or `ReadFile`
+func GetProjectFile(filePath string) string {
+	return filepath.Join(ProjectRoot, filePath)
+}
---
+++ internal/util/project_path_test.go
@@ -0,0 +1,33 @@
+package util
+
+import (
+	"github.com/joho/godotenv"
+	"strings"
+	"testing"
+)
+
+// Mostly just a sanity check since the public
+// funcs are fairly straightforward and location dependent
+func TestProjectPath(t *testing.T) {
+	// SETUP
+	testEnv, err := godotenv.Read("../../.env.test")
+	if err != nil {
+		t.Error("Test Env failed to load")
+	}
+
+	expectedPath := testEnv["EXPECTED_ROOT"] // Expected root folder name (not full path)
+	// WHEN getting `ProjectRoot`, THEN expect it to end with the project root folder name
+	if !strings.HasSuffix(ProjectRoot, expectedPath) {
+		t.Errorf("Project root file path is %v instead of expected %v", ProjectRoot, expectedPath)
+	}
+
+	for _, filePath := range [3]string{"foobar", "/fizz", "//buzz"} {
+		// WHEN trying to get some Project File's path, regardless of leading "/" added
+		actualFilePath := GetProjectFile(filePath)
+		// THEN `filePath` should be properly appended to the Project's root folder path
+		expectedFilePath := expectedPath + "/" + strings.TrimLeft(filePath, "/")
+		if !strings.HasSuffix(actualFilePath, expectedFilePath) {
+			t.Errorf("Actual Path = %v but expected %v", actualFilePath, expectedFilePath)
+		}
+	}
+}
