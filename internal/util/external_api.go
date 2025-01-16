d12616c8f5603a2579b9b31ba9f666a9913f3dfd
NLCaceres <NLCaceres21@Gmail.com>
2025-01-16 12:48:17 -0800

    Fix notes on how `Post`, `Body.Close`, `Read` & `Unmarshal` work
    - Condense usage notes how these functions work under the hood as well
    as how they work together in this `PostRequest` func

--- internal/util/external_api.go
+++ internal/util/external_api.go
@@ -14,15 +14,16 @@ func PostRequest(url string, contentType string, body io.Reader) (map[string]int
 
 	var responseData map[string]interface{}
 
-	response, err := client.Post(url, contentType, body) // Sets the "Content-Type" header for you!
+	response, err := client.Post(url, contentType, body) // DOES set "Content-Type" header
 
 	if err != nil {
 		return responseData, err
 	}
 
-	//INFO: Closing the response body after reading helps the client/server reuse their TCP connection
-	defer response.Body.Close()
-	responseBody, err := io.ReadAll(response.Body)
+	//INFO: Closing the body after Read helps client/server reuse the TCP connection
+	defer response.Body.Close() // Especially if using json.NewDecoder for JSON Streams
+	// Instead of Unmarshal for a normal JSON response albeit costing a bit more memory
+	responseBody, err := io.ReadAll(response.Body) // by reading the whole body
 	if err != nil {
 		return responseData, err
 	}
