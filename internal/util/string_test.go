1044c2aca067606c65f83cdbc8577c7dc9b4d62b
NLCaceres <NLCaceres21@Gmail.com>
2025-01-26 14:23:13 -0800

    Convert `TitleCase` test to a table-test for simplicity
    - Shrinks the test quite a bit and also cleans it up at the same time,
    making it much more readable

--- internal/util/string_test.go
+++ internal/util/string_test.go
@@ -3,42 +3,29 @@ package util
 import "testing"
 
 func TestTitleCase(t *testing.T) {
-	someString := "foo"
-	if TitleCase(someString) != "Foo" {
-		t.Errorf("'%s' did not become 'Foo' as expected", someString)
+	tests := map[string]struct {
+		Input  string
+		Expect string
+	}{ // Go strings can't be nil so an empty check should suffice
+		"An empty string stays empty":     {"", ""},
+		"foo becomes Foo":                 {"foo", "Foo"},
+		"fOO becomes Foo":                 {"fOO", "Foo"},
+		"'foo bar' becomes 'Foo Bar'":     {"foo bar", "Foo Bar"},
+		"'Foo Bar' STAYS 'Foo Bar'":       {"Foo Bar", "Foo Bar"},
+		"'fIzz buZZ' becomes 'Fizz Buzz'": {"fIzz buZZ", "Fizz Buzz"},
+		// Hyphenated words get Titled as individual words
+		"'john-smith' becomes 'John-Smith'": {"john-smith", "John-Smith"},
+		"'John-Smith' STAYS 'John-Smith'":   {"John-Smith", "John-Smith"},
+		// Underscored words get Titled as one word
+		"'jack_jill' becomes 'Jack_jill'": {"jack_jill", "Jack_jill"},
 	}
-
-	if TitleCase("fOO") != "Foo" {
-		t.Error("'fOO' did not become 'Foo' as expected")
+	for testName, testCase := range tests {
+		t.Run(testName, func(t *testing.T) {
+			if TitleCase(testCase.Input) != testCase.Expect {
+				t.Errorf("%q did not become %q as expected", testCase.Input, testCase.Expect)
+			}
+		})
 	}
-
-	if TitleCase("foo bar") != "Foo Bar" {
-		t.Error("'foo bar' did not become 'Foo Bar' as expected")
-	}
-
-	if TitleCase("fIzz buZZ") != "Fizz Buzz" {
-		t.Error("'fIzz buZZ' did not become 'Fizz Buzz' as expected")
-	}
-
-	if TitleCase("john-smith") != "John-Smith" {
-		t.Error("'john-smith' did not become 'John-Smith' as expected")
-	}
-	//NOTE: Hyphenated words Title-Case after hyphens, Underscored words DON'T work similarly
-	if TitleCase("jack_jill") != "Jack_jill" {
-		t.Error("'jack_jill' did not become 'Jack_jill' as expected")
-	}
-
-	if TitleCase("Foo Bar") != "Foo Bar" {
-		t.Error("'Foo Bar' did not stay 'Foo Bar' as expected")
-	}
-	if TitleCase("John-Smith") != "John-Smith" {
-		t.Error("'John-Smith' did not stay 'John-Smith' as expected")
-	}
-
-	if TitleCase("") != "" {
-		t.Error("An empty string did not stay an empty string")
-	}
-	// Explicitly nil values aren't allowed, so should be safe
 }
 
 func TestFindDunderVars(t *testing.T) {
