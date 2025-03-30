cc4ea2e5654b776f7f72d4fe764e4403cb6e1d8c
NLCaceres <NLCaceres21@Gmail.com>
2025-03-30 10:46:32 -0700

    Add alternate handler to serve HTML/templ templates
    - This handler feels a bit more in line with the `Echo` framework by leveraging
    the underlying HTML generated by `templ` and returning it with the proper method
    and desired status code as well as more directly checking and returning errors on failure
      - HOWEVER, it MIGHT be slower due to the change from a `templ` template directly
      written into the response vs a `templ` template converted into a standard library
      `html/template` that is then written into the response

--- internal/handler/views.go
+++ internal/handler/views.go
@@ -2,10 +2,21 @@ package handler
 
 import (
 	"github.com/NLCaceres/goth-example/internal/view"
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 func RenderView(c echo.Context) error {
 	component := view.HTMLIndex(view.Home(), "Home")
 	return component.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func RenderHTMLView(c echo.Context) error {
+	component := view.HTMLIndex(view.Home(), "Home")
+	htmlStr, err := templ.ToGoHTML(c.Request().Context(), component)
+	if err != nil {
+		return c.NoContent(404)
+	}
+	return c.HTML(202, string(htmlStr))
+
 }
