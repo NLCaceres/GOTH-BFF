c8ed2ef34cbd76d441a67e16f14b76aa0174c419
NLCaceres <NLCaceres21@Gmail.com>
2025-04-24 17:21:26 -0700

    Setup table test that uses test spies for assertions
    - Most of the table test setup is the same, i.e. `for` loop on test cases and
    defining the `Input` of the test case; however, this test instead of expecting
    a particular return value, expects the `HttpHandlerFunc` to be called a certain
    number of times in the test. Consequently `ExpectCount` is used in the test case
    struct instead of a simple `Expect`

--- internal/util/test/http_test.go
+++ internal/util/test/http_test.go
@@ -7,9 +7,9 @@ import (
 )
 
 func TestHttpHandlerFunc(t *testing.T) {
-	var tests = map[string]struct {
-		input         HttpMock
-		expectedCount int
+	tests := map[string]struct {
+		Input       HttpMock
+		ExpectCount int // Tracks function calling like a test spy
 	}{
 		"Calls WriteHeader, Write & Header ONCE": {
 			httpMock(200, `{"foo":"bar"}`, map[string]string{"Content-Length": "1"}), 1,
@@ -19,18 +19,18 @@ func TestHttpHandlerFunc(t *testing.T) {
 		}, // NOTE: Adding a test case for when Request Methods ("POST, "GET", etc) don't match ALWAYS fails
 	} // due to underlying `t.Error()` AND the fail CAN'T be prevented, so maybe rewrite Handler later?
 
-	for testName, test := range tests {
+	for testName, testCase := range tests {
 		t.Run(testName, func(t *testing.T) {
-			handler := HttpHandlerFunc(t, test.input)
-			mockRequest, _ := http.NewRequest(test.input.RequestMethod, test.input.RequestURL, bytes.NewBuffer([]byte(``)))
+			handler := HttpHandlerFunc(t, testCase.Input)
+			mockRequest, _ := http.NewRequest(testCase.Input.RequestMethod, testCase.Input.RequestURL, bytes.NewBuffer([]byte(``)))
 			// Setting the Map keys ensures it's checked when the funcs under test are NOT called
 			mockResponseWriter := mockResponseWriter{t, map[string]int{"WriteHeader": 0, "Write": 0, "Header": 0}}
 
 			handler(mockResponseWriter, mockRequest)
 
 			for funcName, count := range mockResponseWriter.CallCounter {
-				if count != test.expectedCount {
-					t.Errorf("ResponseWriter %v() unexpectedly called %d times BUT expected %d times", funcName, count, test.expectedCount)
+				if count != testCase.ExpectCount {
+					t.Errorf("ResponseWriter %v() unexpectedly called %d times BUT expected %d times", funcName, count, testCase.ExpectCount)
 				}
 			}
 		})
