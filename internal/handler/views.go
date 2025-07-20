c22b60470dbdb5c5f5f14c2cabefaf49a2350ee4
NLCaceres <NLCaceres21@Gmail.com>
2025-07-19 23:33:03 -0700

    Make `RenderHTMLView` route handler more flexible for `templ.Components`
    - Updating the function signature to any `templ.Component` and a page
    title makes it possible to render any page-level component in the main
    HTML index file

--- internal/handler/views.go
+++ internal/handler/views.go
@@ -11,12 +11,11 @@ func RenderView(c echo.Context) error {
 	return component.Render(c.Request().Context(), c.Response().Writer)
 }
 
-func RenderHTMLView(c echo.Context) error {
-	component := view.HTMLIndex(view.Home(), "Home")
+func RenderHTMLView(c echo.Context, page templ.Component, title string) error {
+	component := view.HTMLIndex(page, title)
 	htmlStr, err := templ.ToGoHTML(c.Request().Context(), component)
 	if err != nil {
 		return c.NoContent(404)
 	}
 	return c.HTML(202, string(htmlStr))
-
 }
