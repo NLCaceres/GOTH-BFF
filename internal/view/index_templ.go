0f3962d5d80899122429339515d3d8da3197abd1
NLCaceres <NLCaceres21@Gmail.com>
2025-07-20 13:09:34 -0700

    Add a "centered" CSS class to `<main>` in the `index` HTML
    - Takes the content part wrapped by `<main>` of the `index` HTML and centers it!
    - Also ensures that `<main>` takes up the entire viewport's height, which is
    partly based on its content's size.

--- internal/view/index_templ.go
+++ internal/view/index_templ.go
@@ -187,7 +187,7 @@ func HTMLIndex(contents templ.Component, title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<main>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "<main class=\"centered\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
