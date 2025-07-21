2acf949b4353d51842ac52ce6b2d294f19c27201
NLCaceres <NLCaceres21@Gmail.com>
2025-07-21 14:25:47 -0700

    Add "ml-4" CSS class that uses `--spacing` custom property to `itemList` `templ` component
    - Instead of inline CSS styling, I can add in CSS classes that use the `--spacing`
    CSS custom property as needed to handle margins and padding.

--- internal/view/Paths_templ.go
+++ internal/view/Paths_templ.go
@@ -167,14 +167,14 @@ func itemList(
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<br><div style=\"margin-left: 1rem;\">Links to: ")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<br><div class=\"ml-4\">Links to: ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var8 string
 			templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(`"` + expectedLink(item) + `"`)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/Paths.templ`, Line: 37, Col: 79}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/Paths.templ`, Line: 37, Col: 65}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 			if templ_7745c5c3_Err != nil {
