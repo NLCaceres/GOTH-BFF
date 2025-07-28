9c9de77090ba9cddf9d32babf981f689edde2e97
NLCaceres <NLCaceres21@Gmail.com>
2025-07-28 13:26:35 -0700

    Drop `templ.URL` to let `templ` normal URL sanitizing handle it
    - Using `templ.URL` in an "href", "src", or form's "action" attribute is
    unnecessary because `templ` will sanitize AND format the URL automatically for you!
      - In this case, the leading slash added to `navItem` isn't even needed either!
        - Originally called `navItem` simply `item` BUT changed for usage clarity.
        - Adding a leading slash doesn't hurt either, though, so it CAN be included if wanted.

--- internal/view/index_templ.go
+++ internal/view/index_templ.go
@@ -83,16 +83,16 @@ func navbar(navItems []string) templ.Component {
 			return templ_7745c5c3_Err
 		}
 		routeMap := stringy.Map(os.Getenv("ROUTE_MAP"))
-		for _, item := range list.DistinctBy(navItems, strings.ToLower) {
-			if !strings.Contains(item, "-") {
+		for _, navItem := range list.DistinctBy(navItems, strings.ToLower) {
+			if !strings.Contains(navItem, "-") {
 				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 4, "<a href=\"")
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
 				var templ_7745c5c3_Var4 templ.SafeURL
-				templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinURLErrs(templ.URL("/" + item))
+				templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinURLErrs(navItem)
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/index.templ`, Line: 24, Col: 39}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/index.templ`, Line: 24, Col: 25}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 				if templ_7745c5c3_Err != nil {
@@ -103,9 +103,9 @@ func navbar(navItems []string) templ.Component {
 					return templ_7745c5c3_Err
 				}
 				var templ_7745c5c3_Var5 string
-				templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(stringy.PresenterMapValue(routeMap, item))
+				templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(stringy.PresenterMapValue(routeMap, navItem))
 				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/index.templ`, Line: 25, Col: 53}
+					return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/index.templ`, Line: 25, Col: 56}
 				}
 				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 				if templ_7745c5c3_Err != nil {
