0a3905c19ccc76cc72e837b2a7e30fe8d5f288ae
NLCaceres <NLCaceres21@Gmail.com>
2025-07-28 11:42:04 -0700

    Add `MockItems()` to provide test items to render
    - Added `MockItems()` to `Render.HTMLView()` for display in `Paths.templ`

--- internal/model/item.go
+++ internal/model/item.go
@@ -5,3 +5,14 @@ type Item struct {
 	URL         string
 	Description string
 }
+
+func MockItems() []Item {
+	return []Item{
+		{Name: "Fo", URL: "Ba", Description: "Lorem ipsum dolor sit amet"},
+		{Name: "Fi", URL: "Bu", Description: ""},
+		{
+			Name: "Za", URL: "Ze",
+			Description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusm",
+		},
+	}
+}
