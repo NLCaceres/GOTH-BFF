2ca514418eef2f9acdbe0d332ce3caaa4312e5bf
NLCaceres <NLCaceres21@Gmail.com>
2025-07-29 21:17:21 -0700

    Add `path` param into Context for use in `templ` components via the implicit `ctx` var
    - `Templ` components expose an implicit `ctx` variable that is passed in via the
    component's `Render` func, and can grab stored values via `ctx.Value(contextStringKeyName)`
    from anywhere in the view hierarchy, eliminating the need for extensive prop drilling.
      - To add values to the context, in the case of an `Echo` backend, you have to
      mutate the `c.Request().Context()` via `context.WithValue()` which takes the
      request context, a new key string, and its value as arguments.
      - The newly created request context can be passed to `templ.ToGoHTML` which will
      then be passed into the component's `Render` func

--- internal/handler/views.go
+++ internal/handler/views.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"github.com/NLCaceres/goth-example/internal/view"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
@@ -13,9 +14,11 @@ func RenderView(c echo.Context) error {
 
 func RenderHTMLView(c echo.Context, page templ.Component, title string) error {
 	component := view.HTMLIndex(page, title)
-	htmlStr, err := templ.ToGoHTML(c.Request().Context(), component)
+	ctx := context.WithValue(c.Request().Context(), "param", c.Param("name"))
+	htmlStr, err := templ.ToGoHTML(ctx, component)
 	if err != nil {
 		return c.NoContent(404)
 	}
+	// This string conversion should be instant, unlike converting between []byte/string
 	return c.HTML(202, string(htmlStr))
 }
