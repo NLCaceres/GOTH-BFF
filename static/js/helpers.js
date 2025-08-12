3f1f5a2194ef77066b58745058a0c0e2ebc48091
NLCaceres <NLCaceres21@Gmail.com>
2025-08-12 12:48:29 -0700

    Add JSDoc comments to each of the HTML element Javascript helper functions
    - JSDoc comments help to infer argument and return types of the functions as
    well as provide a helpful message on usage via fairly simply structured comments
    placed above the function declaration, similarly to GoDoc comments.

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,7 +1,20 @@
+/** Takes a HTML element and matches the `name` arg with the element's `tagName`
+ * @param {string} name - The expected HTML `tagName`, always in all upper-case form
+ * @return {boolean} `true` if the `tagName` matches the input `name` */
 export const elementIs = (element, name) => element.tagName == name;
 
+/** Takes a HTML element and checks its CSS `classList` for a matching `className`
+ * @param {string} className - CSS class name
+ * @return {boolean} `true` if the class name is found in the element `classList` */
 export const hasClass = (element, className) =>
   element.classList.contains(className);
 
+/**
+ * Takes a HTML element and matches the `name` arg with the element's `tagName`
+ * AND also checks that the element has the input CSS `class`
+ * @param {string} name - The expected HTML `tagName`, always in all upper-case form
+ * @param {string} className - CSS class name
+ * @return {boolean} `true` if both the `name` and CSS `className` match the property
+ * values found in the HTML element `tagName` and `classList`, respectively */
 export const elementWithClass = (element, name, className) =>
   elementIs(element, name) && hasClass(element, className);
