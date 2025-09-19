79556c17c0f87ad2fa7e58075e6453f110f9fc89
NLCaceres <NLCaceres21@Gmail.com>
2025-09-19 10:47:20 -0700

    Update import to `/view/index` and the `HTMLIndex` component

--- internal/handler/views.go
+++ internal/handler/views.go
@@ -1,18 +1,18 @@
 package handler
 
 import (
-	"github.com/NLCaceres/goth-example/internal/view"
+	"github.com/NLCaceres/goth-example/internal/view/index"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 )
 
 func RenderView(c echo.Context) error {
-	component := view.HTMLIndex(view.Home(), "Home", []string{"css/index.css"})
+	component := index.HTMLIndex(index.Home(), "Home", []string{"css/index.css"})
 	return component.Render(c.Request().Context(), c.Response().Writer)
 }
 
 func RenderHTMLView(c echo.Context, page templ.Component, title string, cssPaths []string) error {
-	component := view.HTMLIndex(page, title, cssPaths)
+	component := index.HTMLIndex(page, title, cssPaths)
 	htmlStr, err := templ.ToGoHTML(c.Request().Context(), component)
 	if err != nil {
 		return c.NoContent(404)
