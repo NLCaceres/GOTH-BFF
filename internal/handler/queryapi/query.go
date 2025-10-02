65f27c7780023a868c6fbd25306ab16f65e6cfb2
NLCaceres <NLCaceres21@Gmail.com>
2025-10-02 15:25:34 -0700

    Add struct field tags to `queryapi` models to lower snake_case the Marshaled JSON
    - Couple of exceptions are `SelectBy` field which is converted to `query_by`
    AND `Terms` field which is converted to `searches`
      - Their JSON API names felt less clear, so the Go Field names better
      convey their actual usage in querying for `Item` values in the database.

--- internal/handler/queryapi/query.go
+++ internal/handler/queryapi/query.go
@@ -1,14 +1,14 @@
 package queryapi
 
 type Search struct {
-	FilterBy string
-	Page     int
-	PerPage  int
-	Q        string
-	SelectBy string
-	SortBy   string
+	FilterBy string `json:"filter_by"`
+	Page     int    `json:"page"`
+	PerPage  int    `json:"per_page"`
+	Q        string `json:"q"`
+	SelectBy string `json:"query_by"`
+	SortBy   string `json:"sort_by"`
 }
 
 type Request struct {
-	Terms []Search
+	Terms []Search `json:"searches"`
 }
