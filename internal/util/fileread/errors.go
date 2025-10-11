8b6b22e21d33acde91688fb4417c68e0ff5a4bcb
NLCaceres <NLCaceres21@Gmail.com>
2025-10-10 17:36:47 -0700

    Create a `FileReadError` to act as a parent error to unwrap into the other `fileread` errors
    - Includes a doc comment to help convey this intended usage, and how it relates
    to Go's philosophy on error propagation and handling given that they're simple
    values unlike in languages like Java with checked/unchecked errors.

--- internal/util/fileread/errors.go
+++ internal/util/fileread/errors.go
@@ -9,6 +9,20 @@ var (
 	WrongFileTypeError = errors.New("Incorrect File Type: Expected \".json\" file")
 )
 
+// An `error` type that is returned when attempting to read a file, particularly JSON files
+// Takes an `Err` field that it unwraps to form an error chain to simulate inheritance
+// Namely, FileNotFoundError and MalformedJsonError should be input as "descendants"
+type FileReadError struct {
+	Err error
+}
+
+func (e FileReadError) Error() string {
+	return fmt.Sprintf("FileRead Error: %v", e.Err)
+}
+func (e FileReadError) Unwrap() error {
+	return e.Err
+}
+
 type FileNotFoundError struct {
 	File string
 }
