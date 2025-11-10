5b677588bebb3e6743caa2db302684737f60206a
NLCaceres <NLCaceres21@Gmail.com>
2025-11-09 23:31:02 -0800

    Move `queryapi.Request` types into new `/queryapi/request.go` to split concerns

--- internal/handler/queryapi/query.go
+++ internal/handler/queryapi/query.go
@@ -1,10 +1,7 @@
 package queryapi
 
 import (
-	"errors"
 	"github.com/NLCaceres/goth-example/internal/util/datetime"
-	"github.com/NLCaceres/goth-example/internal/util/stringy"
-	"strings"
 )
 
 type Response struct {
@@ -64,38 +61,4 @@ type Document struct {
 	PostTime datetime.Local `json:"posted"` // Tends to get microsecond precision
 	Title    string         `json:"title"`
 	URL      string         `json:"url"`
-}
-
-type Search struct {
-	FilterBy string `json:"filter_by"`
-	Page     int    `json:"page"`
-	PerPage  int    `json:"per_page"`
-	Q        string `json:"q"`
-	SelectBy string `json:"query_by"`
-	SortBy   string `json:"sort_by"`
-}
-
-var SearchSetterError = errors.New("Unable to set Search Filter Field")
-
-func (s *Search) setFilters(filters string) error {
-	matches, err := stringy.FindDunderVars(s.FilterBy)
-	if err != nil {
-		return SearchSetterError
-	}
-	replacements := strings.Split(filters, "|")
-	for i := range min(len(replacements), len(matches)) {
-		s.FilterBy = strings.Replace(s.FilterBy, matches[i], replacements[i], 1)
-	}
-	return nil
-}
-
-type Request struct {
-	Terms []Search `json:"searches"`
-}
-
-func (r Request) last() *Search {
-	if len(r.Terms) == 0 {
-		return nil
-	}
-	return &r.Terms[len(r.Terms)-1]
 }
---
+++ internal/handler/queryapi/request.go
@@ -0,0 +1,41 @@
+package queryapi
+
+import (
+	"errors"
+	"github.com/NLCaceres/goth-example/internal/util/stringy"
+	"strings"
+)
+
+type Request struct {
+	Terms []Search `json:"searches"`
+}
+
+func (r Request) last() *Search {
+	if len(r.Terms) == 0 {
+		return nil
+	}
+	return &r.Terms[len(r.Terms)-1]
+}
+
+type Search struct {
+	FilterBy string `json:"filter_by"`
+	Page     int    `json:"page"`
+	PerPage  int    `json:"per_page"`
+	Q        string `json:"q"`
+	SelectBy string `json:"query_by"`
+	SortBy   string `json:"sort_by"`
+}
+
+var SearchSetterError = errors.New("Unable to set Search Filter Field")
+
+func (s *Search) setFilters(filters string) error {
+	matches, err := stringy.FindDunderVars(s.FilterBy)
+	if err != nil {
+		return SearchSetterError
+	}
+	replacements := strings.Split(filters, "|")
+	for i := range min(len(replacements), len(matches)) {
+		s.FilterBy = strings.Replace(s.FilterBy, matches[i], replacements[i], 1)
+	}
+	return nil
+}
