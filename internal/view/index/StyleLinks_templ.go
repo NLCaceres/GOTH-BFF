4ad3ac3b932ebbbbf84f54abe5b3fadd61cb0bf9
NLCaceres <NLCaceres21@Gmail.com>
2026-01-06 23:27:39 -0800

    Fix link to CSS for position, padding and margins
    - Updates to new name as `layout.css`

--- internal/view/index/StyleLinks_templ.go
+++ internal/view/index/StyleLinks_templ.go
@@ -53,7 +53,7 @@ func StyleLinks() templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = StyleLink("css/spacing.css").Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = StyleLink("css/layout.css").Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
