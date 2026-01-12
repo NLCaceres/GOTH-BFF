ec2dbbe4e26b169aecad8ad29931a75ea18b7244
NLCaceres <NLCaceres21@Gmail.com>
2026-01-12 10:50:46 -0800

    Add font-weight, margin and background color CSS helpers to `list` component ID
    - Margin helps with spacing between items particularly for mobile viewports.
    - Item Name/ID starts as a simple red background color BUT should transform to
    green, so a `bg-green` CSS helper class has been added as well.
    - Font-weight CSS helper corresponds to `font-weight: bolder;` to help accentuate ID

--- internal/view/items/List_templ.go
+++ internal/view/items/List_templ.go
@@ -89,14 +89,14 @@ func list(items []model.Item) templ.Component {
 			return templ_7745c5c3_Err
 		}
 		for i, item := range items {
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<li class=\"grid listItem grid-transition mb-4\"><div class=\"itemName flex centered cross-centered bg-gray-700\"><div class=\"itemNameCover flex centered cross-centered\"><p class=\"itemNameID text-xl\">")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 5, "<li class=\"grid listItem grid-transition mb-12\"><div class=\"itemName flex centered cross-centered bg-gray-700\"><div class=\"itemNameCover flex centered cross-centered bg-red\"><p class=\"itemNameID text-xl fw-bolder\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(i + 1)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/items/List.templ`, Line: 23, Col: 49}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/items/List.templ`, Line: 23, Col: 59}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
