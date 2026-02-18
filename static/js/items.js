1e90c2c3ce376e7b9e16c6f0672566265c31eb76
NLCaceres <NLCaceres21@Gmail.com>
2026-02-18 00:37:10 -0800

    Drop ellipsis event handlers for simple `overflowing` CSS class init func
    - Since no longer using an ellipsis to handle obscuring long texts, I can not only
    drop the event handlers used to smoothen the effect, but also setup a loop in its
    place to add the CSS class for the new linear-gradient version on all items that need it.

--- static/js/items.js
+++ static/js/items.js
@@ -1,41 +1,3 @@
-import { elementWithClass } from "./helpers.js";
-
-/* Mouse event handlers to simulate a CSS hover effect */
-function mouseEntered(event) {
-  if (!elementWithClass(event.target, "DIV", "itemLink")) {
-    return;
-  }
-  event.target.dataset.expanding = true;
-  delete event.target.dataset.closed;
-}
-function mouseLeft(event) {
-  if (!elementWithClass(event.target, "DIV", "itemLink")) {
-    return;
-  }
-  event.target.dataset.closed = true;
-  delete event.target.dataset.expanding;
-}
-
-/* Transition Event handlers to help smoothen the CSS hover effect */
-function transitionStarted(event) {
-  if (!elementWithClass(event.target, "LI", "listItem")) {
-    return;
-  }
-  const link = event.target.querySelector(".itemLink");
-  if (link.dataset.expanding === "true") {
-    link.classList.add("no-ellipsis");
-  }
-}
-function transitionEnded(event) {
-  if (!elementWithClass(event.target, "LI", "listItem")) {
-    return;
-  }
-  const link = event.target.querySelector(".itemLink");
-  if (link.dataset.closed === "true") {
-    link.classList.remove("no-ellipsis");
-  }
-}
-
 function checkBoxClicked(e) {
   const item = e.currentTarget;
   const id = item.parentElement?.previousElementSibling?.previousElementSibling?.firstElementChild;
@@ -50,12 +12,11 @@ function checkBoxClicked(e) {
   }
 }
 
-/* Runs when <script> loads this file, adding event handlers IF it finds matching elems */
-for (const item of document.getElementsByClassName("items")) {
-  item.addEventListener("mouseenter", mouseEntered, true);
-  item.addEventListener("mouseleave", mouseLeft, true);
-  item.addEventListener("transitionstart", transitionStarted, true);
-  item.addEventListener("transitionend", transitionEnded, true);
+/* Following code runs when the `<script>` is loaded */
+for (const item of document.getElementsByClassName("itemLink")) {
+  if ((item.scrollHeight - item.offsetHeight) > 5) {
+    item.lastElementChild.classList.add("overflowing");
+  }
 }
 
 for (const checkBox of document.getElementsByClassName("checkbox-button")) {
