ffbc8f350ec7cc1f13e3240571d08e5227f96930
NLCaceres <NLCaceres21@Gmail.com>
2026-02-17 23:36:46 -0800

    Drop `line-clamp` based ellipsis for `linear-gradient` obscured version
    - Using a linear-gradient to obscure the end text of a `<p>` element is more
    flexible since it leverages `position` to always correctly float above the text
    regardless of child HTML structure.
    - It's also better supported across browsers, in spite of using `:has()`
    since `line-clamp` support is still a bit shakey.

--- internal/view/items/List_templ.go
+++ internal/view/items/List_templ.go
@@ -102,7 +102,7 @@ func list(items []model.Item) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "</p></div></div><div class=\"itemLink flex flex-column bg-gray-700 hide-overflow text-line-3 py-1\"><a href=\"")
+			templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 6, "</p></div></div><div class=\"itemLink flex flex-column bg-gray-700 hide-overflow py-1\"><a href=\"")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
