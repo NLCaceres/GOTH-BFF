bf26b2e64a0aff464bdc5d7fe8b66c8ac85b89b8
NLCaceres <NLCaceres21@Gmail.com>
2026-02-25 22:56:21 -0800

    Restore `prettier` settings and config with test on `items.js`
    - Wasn't properly formatting on save as expected, so formatting `items.js`
    to expected result after small `.prettierrc.json` update

--- static/js/items.js
+++ static/js/items.js
@@ -3,7 +3,7 @@ import { debounce } from "./helpers.js";
 /** Checks all `.itemLink` elems, adding CSS marker class if <p> inside overflows */
 function itemLinkOverflowing() {
   for (const item of document.getElementsByClassName("itemLink")) {
-    if ((item.scrollHeight - item.offsetHeight) > 5) {
+    if (item.scrollHeight - item.offsetHeight > 5) {
       item.lastElementChild.classList.add("overflowing");
     } else {
       item.lastElementChild.classList.remove("overflowing");
@@ -17,22 +17,23 @@ window.addEventListener("resize", debounce(itemLinkOverflowing, 500));
 /** Syncs the ID container color with the checkbox background color in `.listItem` elem */
 function syncItemColoring(e) {
   const checkbox = e.currentTarget;
-  const id = checkbox.parentElement?.previousElementSibling?.previousElementSibling?.firstElementChild;
+  const id =
+    checkbox.parentElement?.previousElementSibling?.previousElementSibling
+      ?.firstElementChild;
   const checkboxBackground = checkbox.previousElementSibling?.firstElementChild;
-  if (!checkbox.classList.contains('check')) {
-    id?.classList.replace('bg-green', 'bg-red');
-    checkboxBackground?.setAttribute('stroke', 'red');
-  }
-  else {
-    id?.classList.replace('bg-red', 'bg-green');
-    checkboxBackground?.setAttribute('stroke', 'green');
+  if (!checkbox.classList.contains("check")) {
+    id?.classList.replace("bg-green", "bg-red");
+    checkboxBackground?.setAttribute("stroke", "red");
+  } else {
+    id?.classList.replace("bg-red", "bg-green");
+    checkboxBackground?.setAttribute("stroke", "green");
   }
 }
 
 // Runs as soon the HTML is parsed BUT often before it's fully constructed
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   itemLinkOverflowing();
   for (const checkBox of document.getElementsByClassName("checkbox-button")) {
-    checkBox.addEventListener('click', syncItemColoring);
+    checkBox.addEventListener("click", syncItemColoring);
   }
 });
