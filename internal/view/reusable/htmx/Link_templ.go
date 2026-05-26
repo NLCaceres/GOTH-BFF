2d9304a3519644a210b7c2594cd10dc79c565f67
NLCaceres <NLCaceres21@Gmail.com>
2026-05-26 14:59:52 -0700

    Test click delay on the HTMX Link component
    - The click delay works like debounce BUT without a leading call, so 500ms
    must elapse between clicks before even the first request is triggered.

--- internal/view/reusable/htmx/Link_templ.go
+++ internal/view/reusable/htmx/Link_templ.go
@@ -55,14 +55,14 @@ func Link(name string, url string, target string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-push-url=\"true\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-push-url=\"true\" hx-trigger=\"click delay:500ms\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var4 string
 		templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/reusable/htmx/Link.templ`, Line: 4, Col: 69}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `internal/view/reusable/htmx/Link.templ`, Line: 5, Col: 61}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 		if templ_7745c5c3_Err != nil {
