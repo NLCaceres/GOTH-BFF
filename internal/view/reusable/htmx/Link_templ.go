e08ede530b6c2c1a205347bef816a295f30bf102
NLCaceres <NLCaceres21@Gmail.com>
2026-05-26 15:18:24 -0700

    Test throttle on the HTMX Link component
    - Works a bit more the way I want, emitting a request immediately before allowing
    another 1 second later.
      - Using it in combination with `delay` doesn't work great for my use case, since
      the request fires off 500ms later, throttle lets another request at 1s mark, and
      then it fires off at the 1.5s mark

--- internal/view/reusable/htmx/Link_templ.go
+++ internal/view/reusable/htmx/Link_templ.go
@@ -55,7 +55,7 @@ func Link(name string, url string, target string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-push-url=\"true\" hx-trigger=\"click delay:500ms\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "\" hx-push-url=\"true\" hx-trigger=\"click throttle:1s\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
