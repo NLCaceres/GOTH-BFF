1ce24ac9062b654c29c4fedc7851bc38f59fb789
NLCaceres <NLCaceres21@Gmail.com>
2026-05-28 00:12:07 -0700

    Fix `static` directory middleware to allow `127.0.0.1`
    - If used `127.0.0.1` instead of `localhost`, then assets wouldn't be available

--- internal/middleware/static.go
+++ internal/middleware/static.go
@@ -12,6 +12,7 @@ func Static() echo.MiddlewareFunc {
 		Root: "static",
 		Skipper: func(c *echo.Context) bool { // Skip if returning true
 			isValidRef := strings.HasPrefix(c.Request().Referer(), "http://localhost:3000") ||
+				strings.HasPrefix(c.Request().Referer(), "http://127.0.0.1:3000") ||
 				strings.HasPrefix(c.Request().Referer(), "http://127.0.0.1:7331") ||
 				strings.HasPrefix(c.Request().Referer(), "https://localhost:3000")
 			isNavigation := slices.Contains(c.Request().Header["Sec-Fetch-Mode"], "navigate")
