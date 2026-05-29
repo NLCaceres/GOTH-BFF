37896a56740c7a3e38bd8f77f0b52422ebb7dde6
NLCaceres <NLCaceres21@Gmail.com>
2026-05-29 12:20:05 -0700

    Replace Static middleware check vars with inline checks in return
    - This could help speed up the middleware and return static assets more quickly
    by failing the check fast AND it puts off the `HasAnyPrefix` check until the end.
    It does, however, come at the cost of readability, so funcs/methods should help.

--- internal/middleware/static.go
+++ internal/middleware/static.go
@@ -10,14 +10,10 @@ func Static() echo.MiddlewareFunc {
 	return middleware.StaticWithConfig(middleware.StaticConfig{
 		Root: "static",
 		Skipper: func(c *echo.Context) bool { // Skip if returning true
-			isValidRef := stringy.HasAnyPrefix(c.Request().Referer(), "http://localhost:3000",
-				"http://127.0.0.1:3000", "http://127.0.0.1:7331", "https://localhost:3000")
-			isNavigation := c.Request().Header.Get("Sec-Fetch-Mode") == "navigate"
-			isSameOrigin := c.Request().Header.Get("Sec-Fetch-Site") == "same-origin"
-			if isValidRef && !isNavigation && isSameOrigin {
-				return false
-			}
-			return true
+			return !(c.Request().Header.Get("Sec-Fetch-Site") == "same-origin" &&
+				!(c.Request().Header.Get("Sec-Fetch-Mode") == "navigate") &&
+				stringy.HasAnyPrefix(c.Request().Referer(), "http://localhost:3000",
+					"https://localhost:3000", "http://127.0.0.1:3000", "http://127.0.0.1:7331"))
 		},
 	})
 }
