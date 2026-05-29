b5d70df6c5a223ec0fe1db1eb8a8a3ebc2238cb4
NLCaceres <NLCaceres21@Gmail.com>
2026-05-29 11:53:29 -0700

    Further simplify the Static middleware to do less "Fetch" header checks
    - Using the `Get` method on `http.Header` type grabs the first value in a Header,
    which in the case of "Sec-Fetch-Mode" and "Sec-Fetch-Site" should ONLY ever have
    1 value, which means I can get rid of `slices.Contains` for a simple "==" comparison.

--- internal/middleware/static.go
+++ internal/middleware/static.go
@@ -4,7 +4,6 @@ import (
 	"github.com/NLCaceres/goth-example/internal/util/stringy"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
-	"slices"
 )
 
 func Static() echo.MiddlewareFunc {
@@ -13,8 +12,8 @@ func Static() echo.MiddlewareFunc {
 		Skipper: func(c *echo.Context) bool { // Skip if returning true
 			isValidRef := stringy.HasAnyPrefix(c.Request().Referer(), "http://localhost:3000",
 				"http://127.0.0.1:3000", "http://127.0.0.1:7331", "https://localhost:3000")
-			isNavigation := slices.Contains(c.Request().Header["Sec-Fetch-Mode"], "navigate")
-			isSameOrigin := slices.Contains(c.Request().Header["Sec-Fetch-Site"], "same-origin")
+			isNavigation := c.Request().Header.Get("Sec-Fetch-Mode") == "navigate"
+			isSameOrigin := c.Request().Header.Get("Sec-Fetch-Site") == "same-origin"
 			if isValidRef && !isNavigation && isSameOrigin {
 				return false
 			}
