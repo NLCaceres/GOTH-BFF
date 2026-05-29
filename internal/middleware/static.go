92ccc6e03b10c788eb72be1a3f8bc276523a8fd3
NLCaceres <NLCaceres21@Gmail.com>
2026-05-29 00:44:37 -0700

    Update static middleware to use simpler singular `HasAnyPrefix` call
    - Rather than chaining `HasPrefix` conditions into a bool, `HasAnyPrefix`
    forms a much nicer 2-liner

--- internal/middleware/static.go
+++ internal/middleware/static.go
@@ -1,20 +1,18 @@
 package middleware
 
 import (
+	"github.com/NLCaceres/goth-example/internal/util/stringy"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
 	"slices"
-	"strings"
 )
 
 func Static() echo.MiddlewareFunc {
 	return middleware.StaticWithConfig(middleware.StaticConfig{
 		Root: "static",
 		Skipper: func(c *echo.Context) bool { // Skip if returning true
-			isValidRef := strings.HasPrefix(c.Request().Referer(), "http://localhost:3000") ||
-				strings.HasPrefix(c.Request().Referer(), "http://127.0.0.1:3000") ||
-				strings.HasPrefix(c.Request().Referer(), "http://127.0.0.1:7331") ||
-				strings.HasPrefix(c.Request().Referer(), "https://localhost:3000")
+			isValidRef := stringy.HasAnyPrefix(c.Request().Referer(), "http://localhost:3000",
+				"http://127.0.0.1:3000", "http://127.0.0.1:7331", "https://localhost:3000")
 			isNavigation := slices.Contains(c.Request().Header["Sec-Fetch-Mode"], "navigate")
 			isSameOrigin := slices.Contains(c.Request().Header["Sec-Fetch-Site"], "same-origin")
 			if isValidRef && !isNavigation && isSameOrigin {
