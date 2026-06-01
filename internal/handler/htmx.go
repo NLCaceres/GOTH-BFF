589b4580cee38cd6ff9ff55a57cc2d59537b9e74
NLCaceres <NLCaceres21@Gmail.com>
2026-05-31 19:31:28 -0700

    Add HTTP header checks to `HtmxPayload` via new helper + `RenderMode` type
    - The new helper checks HTTP "Referer", "Sec-Fetch-Mode", and "Sec-Fetch-Site"
    headers to determine the payload required, returning a `RenderMode` type def string
    for easy checking in a `switch` statement to return either the full page or HTMX component

--- internal/handler/htmx.go
+++ internal/handler/htmx.go
@@ -2,21 +2,48 @@ package handler
 
 import (
 	"github.com/NLCaceres/goth-example/internal/model"
+	"github.com/NLCaceres/goth-example/internal/util/stringy"
 	"github.com/NLCaceres/goth-example/internal/view/index"
 	"github.com/NLCaceres/goth-example/internal/view/items"
 	"github.com/NLCaceres/goth-example/internal/view/reusable"
 	"github.com/labstack/echo/v5"
+	"net/http"
 )
 
 func HtmxPayload(c *echo.Context, name string) error {
-	switch fetchMode := c.Request().Header.Get("Sec-Fetch-Mode"); fetchMode {
-	case "navigate":
+	switch renderMode := getRenderMode(c.Request().Header); renderMode {
+	case Full:
 		vm := index.ViewModel{Title: name, CssPaths: []string{"css/item_list.css"}}
 		itemsVm := items.ViewModel{Title: name, Items: model.ManyMockItems()}
 		return RenderHTMLIndex(c, items.ListPage(itemsVm), vm)
-	case "cors", "same-origin":
+	case HTMX:
 		return RenderHTML(c, reusable.TestElem(name))
 	default:
 		return RenderHTML(c, reusable.TestElem("Error!"))
+	}
+}
+
+type RenderMode string
+
+const (
+	Full  RenderMode = "full"
+	HTMX  RenderMode = "htmx"
+	Error RenderMode = "error"
+)
+
+func getRenderMode(h http.Header) RenderMode {
+	referer := h.Get("Referer")
+	fetchMode := h.Get("Sec-Fetch-Mode")
+	fetchSite := h.Get("Sec-Fetch-Site")
+	switch {
+	case (fetchMode == "navigate" &&
+		(fetchSite == "same-origin" || fetchSite == "same-site")) || referer == "":
+		return Full
+	case stringy.HasAnyPrefix(referer, "http://localhost") ||
+		((fetchMode == "cors" || fetchMode == "same-origin") &&
+			(fetchSite == "same-origin" || fetchSite == "same-site")):
+		return HTMX
+	default:
+		return Error
 	}
 }
