3760286c22bb57fda2a03d921d1d6c913ad7fe10
NLCaceres <NLCaceres21@Gmail.com>
2026-06-07 17:02:25 -0700

    Condense `LoadingIndicator` CSS into ID selector EXCEPT `p-center`
    - Easier to condense down several single-line CSS classes into a more
    specific CSS style selector BUT leave `p-center` the more elaborate CSS helper class
    - Helps to add a max-width media query for `t-25`
    - Also focused the `transition` CSS property to just `opacity` and `visibility`

--- internal/view/reusable/htmx/LoadingIndicator_templ.go
+++ internal/view/reusable/htmx/LoadingIndicator_templ.go
@@ -32,7 +32,7 @@ func LoadingIndicator() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"load-indicator\" class=\"htmx-indicator p-absolute p-center t-25 z-over text-white transitionable\">Loading!</div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<div id=\"load-indicator\" class=\"htmx-indicator p-center\">Loading!</div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
