9c28220bdeba47df762dbe35b6be2ed7e83e1122
NLCaceres <NLCaceres21@Gmail.com>
2026-06-14 00:41:08 -0700

    Replace custom `http.Header` with `htmx.Header` and its new init helper in `HtmxPayload`
    - The partial/fragment HTMX payload param is renamed to `partial` instead of `htmx`
    - First case in `isFullRender` is `h.IsHxRequest` returning `false, nil` like the
    CORS/same-origin/same-site check

--- internal/handler/htmx.go
+++ internal/handler/htmx.go
@@ -2,14 +2,15 @@ package handler
 
 import (
 	"fmt"
+	"github.com/NLCaceres/goth-example/internal/util/htmx"
 	myHttp "github.com/NLCaceres/goth-example/internal/util/http"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v5"
 	"net/http"
 )
 
-func HtmxPayload(c *echo.Context, fullPage, htmx templ.Component) error {
-	isFullRender, err := isFullRender(myHttp.Header{Header: c.Request().Header})
+func HtmxPayload(c *echo.Context, fullPage, partial templ.Component) error {
+	isFullRender, err := isFullRender(htmx.NewHeader(c.Request().Header))
 	if err != nil { // "Hx-Location" can cause infinite re-renders
 		c.Response().Header().Add("Hx-Location", `{"path":"/error", "target":"main"}`)
 		return c.NoContent(http.StatusBadRequest)
@@ -17,16 +18,18 @@ func HtmxPayload(c *echo.Context, fullPage, htmx templ.Component) error {
 	if isFullRender {
 		return RenderHTML(c, fullPage)
 	} else {
-		return RenderHTML(c, htmx)
+		return RenderHTML(c, partial)
 	}
 }
 
-func isFullRender(h myHttp.Header) (bool, error) {
+func isFullRender(h htmx.Header) (bool, error) {
 	switch {
+	case h.IsHxRequest():
+		return false, nil
 	case (h.IsNavigationFetch() && h.IsSiteNone()) || !h.HasReferer():
 		return true, nil
-	case ((h.IsCORSFetch() || h.IsSameOriginFetch()) &&
-		h.IsSiteAllSame()) || h.IsRefererAny(myHttp.SafeOrigins...):
+	case ((h.IsCORSFetch() || h.IsSameOriginFetch()) && h.IsSiteAllSame()) ||
+		h.IsRefererAny(myHttp.SafeOrigins...):
 		return false, nil
 	default:
 		return false,
