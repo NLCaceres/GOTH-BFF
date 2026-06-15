4dad1a7ccfbfa048ac198b2b6f9fc47ea293b171
NLCaceres <NLCaceres21@Gmail.com>
2026-06-15 00:38:20 -0700

    Rename `util/http.Header` import alias to `apphttp` + clean render/header cases
    - `apphttp` feels more in line with Go package naming convention over `myHttp`
    - For now, `IsHxRequest` condition can be combined with the CORS/origin/site
    check since they return the same `false, nil` BUT might eventually decide to make
    `isFullRender` into a simple `h.IsHxRequest`
    - Update error messaging to reflect `HxRequest` check

--- internal/handler/htmx.go
+++ internal/handler/htmx.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"fmt"
 	"github.com/NLCaceres/goth-example/internal/util/htmx"
-	myHttp "github.com/NLCaceres/goth-example/internal/util/http"
+	apphttp "github.com/NLCaceres/goth-example/internal/util/http"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v5"
 	"net/http"
@@ -24,16 +24,14 @@ func HtmxPayload(c *echo.Context, fullPage, partial templ.Component) error {
 
 func isFullRender(h htmx.Header) (bool, error) {
 	switch {
-	case h.IsHxRequest():
+	case h.IsHxRequest() ||
+		((h.IsCORSFetch() || h.IsSameOriginFetch()) && h.IsSiteAllSame()) || h.IsRefererAny(apphttp.SafeOrigins...):
 		return false, nil
 	case (h.IsNavigationFetch() && h.IsSiteNone()) || !h.HasReferer():
 		return true, nil
-	case ((h.IsCORSFetch() || h.IsSameOriginFetch()) && h.IsSiteAllSame()) ||
-		h.IsRefererAny(myHttp.SafeOrigins...):
-		return false, nil
 	default:
 		return false,
-			fmt.Errorf("Issue determining if full render needed: FetchMode = %v  "+
-				"FetchSite = %v  Referer = %v", h.FetchMode(), h.FetchSite(), h.Referer())
+			fmt.Errorf("Issue determining if full render needed: HxRequest = %v  FetchMode = %v"+
+				"  FetchSite = %v  Referer = %v", h.HxRequest(), h.FetchMode(), h.FetchSite(), h.Referer())
 	}
 }
