d2622b8d49c1807699c34041d3052aa4892aa1fa
NLCaceres <NLCaceres21@Gmail.com>
2026-06-17 23:56:04 -0700

    Add wrapper type for `templ.Component` to more easily build HTMX responses
    - `htmx.PageComponent` has an `AddTitle` and `AddStyle` func to facilitate out-of-band
    swaps for `<title>` and targeted `<style>` elements

---
+++ internal/view/reusable/htmx/Component.go
@@ -0,0 +1,25 @@
+package htmx
+
+import "github.com/a-h/templ"
+
+// Wrapper for templ.Component to add helpful methods to more easily build HTMX component responses
+type PageComponent struct {
+	templ.Component
+}
+
+// Initializer for PageComponent to wrap a templ.Component and deliver as HTMX payload
+func Data(c templ.Component) PageComponent {
+	return PageComponent{c}
+}
+
+// Fluent API to templ.Join the main wrapped component with a Title element so HTMX
+// can update the HTML <title> via auto-magic out-of-band swap
+func (c PageComponent) AddTitle(title string) PageComponent {
+	return PageComponent{templ.Join(c, Title(title))}
+}
+
+// Fluent API to templ.Join the main wrapped component with a Style element so HTMX
+// can update via out-of-band swap any matching target, ideally in the <head>
+func (c PageComponent) AddStyle(path string) PageComponent {
+	return PageComponent{templ.Join(c, StyleLink(path))}
+}
