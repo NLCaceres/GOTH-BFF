a8ba036d22d2e41f44ab037a46e5783382d5d5e7
NLCaceres <NLCaceres21@Gmail.com>
2026-06-18 23:36:54 -0700

    Condense `HtmxPayload` into simple `IsHxRequest` if/else
    - While the other checks could work/help, they do fail when a link is clicked
    that doesn't use my `reusable/htmx.Link`, making it simpler to rely on HTMX modifying
    request headers with `Hx-Request` so all other links can work normally (no boosting either)

--- internal/handler/htmx.go
+++ internal/handler/htmx.go
@@ -1,37 +1,15 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/NLCaceres/goth-example/internal/util/htmx"
-	apphttp "github.com/NLCaceres/goth-example/internal/util/http"
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v5"
-	"net/http"
 )
 
 func HtmxPayload(c *echo.Context, fullPage, partial templ.Component) error {
-	isFullRender, err := isFullRender(htmx.NewHeader(c.Request().Header))
-	if err != nil {
-		htmx.NewHeader(c.Response().Header()).AddLocation("/error", "main")
-		return c.NoContent(http.StatusBadRequest)
-	}
-	if isFullRender {
-		return RenderHTML(c, fullPage)
-	} else {
+	if htmx.NewHeader(c.Request().Header).IsHxRequest() {
 		return RenderHTML(c, partial)
-	}
-}
-
-func isFullRender(h htmx.Header) (bool, error) {
-	switch {
-	case h.IsHxRequest() ||
-		((h.IsCORSFetch() || h.IsSameOriginFetch()) && h.IsSiteAllSame()) || h.IsRefererAny(apphttp.SafeOrigins...):
-		return false, nil
-	case (h.IsNavigationFetch() && h.IsSiteNone()) || !h.HasReferer():
-		return true, nil
-	default:
-		return false,
-			fmt.Errorf("Issue determining if full render needed: HxRequest = %v  FetchMode = %v"+
-				"  FetchSite = %v  Referer = %v", h.HxRequest(), h.FetchMode(), h.FetchSite(), h.Referer())
+	} else {
+		return RenderHTML(c, fullPage)
 	}
 }
