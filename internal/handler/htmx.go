4ed5835fadb111ad8ddc10ccadb8b9456f1c40ad
NLCaceres <NLCaceres21@Gmail.com>
2026-06-20 15:08:50 -0700

    Split component `ToHTML` and error handling into `RenderHtmx` func
    - Helps to leave `HtmxPayload` to deciding whether to send a partial or full page
    while the `RenderHtmx` func can do the conversion to the HTML string and decide
    whether to return a 200 with HTML or return a 404

--- internal/handler/htmx.go
+++ internal/handler/htmx.go
@@ -6,15 +6,21 @@ import (
 	"net/http"
 )
 
+// Takes a full page component or the `Hx-Swap` ready partial component and decides
+// based on `Hx-` prefixed headers which to send back as a response.
 func HtmxPayload(c *echo.Context, fullPage, partial htmx.PageComponent) error {
-	html, err := "", error(nil)
-
 	if htmx.NewHeader(c.Request().Header).IsHxRequest() {
-		html, err = partial.ToHTML(c.Request().Context())
+		return RenderHtmx(c, partial)
 	} else {
-		html, err = fullPage.ToHTML(c.Request().Context())
+		return RenderHtmx(c, fullPage)
 	}
+}
 
+// Takes a htmx.PageComponent wrapping a templ.Component to convert into an HTML string.
+// If the conversion fails, an empty 404 status code is returned.
+// If the conversion succeeds, the HTML is returned with status code 200.
+func RenderHtmx(c *echo.Context, component htmx.PageComponent) error {
+	html, err := component.ToHTML(c.Request().Context())
 	if err != nil {
 		return c.NoContent(http.StatusNotFound)
 	}
