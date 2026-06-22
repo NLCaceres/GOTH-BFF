09e324a104f26a2c54b4a225a56e1b76c47a26bf
NLCaceres <NLCaceres21@Gmail.com>
2026-06-21 23:39:14 -0700

    Move `handler/htmx.go` into its own package `handler/htmx` pkg
    - Probably best to group all `util/htmx` files into the same `handler/htmx` pkg
    in particular as they all relate to forming a HTTP response
      - Also helps avoid a circular import

--- internal/handler/htmx/htmx.go
+++ internal/handler/htmx/htmx.go
@@ -1,25 +1,24 @@
-package handler
+package htmx
 
 import (
-	"github.com/NLCaceres/goth-example/internal/util/htmx"
 	"github.com/labstack/echo/v5"
 	"net/http"
 )
 
 // Takes a full page component or the `Hx-Swap` ready partial component and decides
 // based on `Hx-` prefixed headers which to send back as a response.
-func HtmxPayload(c *echo.Context, fullPage, partial htmx.PageComponent) error {
-	if htmx.NewHeader(c.Request().Header).IsHxRequest() {
+func HtmxPayload(c *echo.Context, fullPage, partial PageComponent) error {
+	if NewHeader(c.Request().Header).IsHxRequest() {
 		return RenderHtmx(c, partial)
 	} else {
 		return RenderHtmx(c, fullPage)
 	}
 }
 
-// Takes a htmx.PageComponent wrapping a templ.Component to convert into an HTML string.
+// Takes a PageComponent wrapping a templ.Component to convert into an HTML string.
 // If the conversion fails, an empty 404 status code is returned.
 // If the conversion succeeds, the HTML is returned with status code 200.
-func RenderHtmx(c *echo.Context, component htmx.PageComponent) error {
+func RenderHtmx(c *echo.Context, component PageComponent) error {
 	html, err := component.ToHTML(c.Request().Context())
 	if err != nil {
 		return c.NoContent(http.StatusNotFound)
