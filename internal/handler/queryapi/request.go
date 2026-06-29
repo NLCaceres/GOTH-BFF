4f3ab379d8e8ea48a716acd5b4b9170f02b0bf0e
NLCaceres <NLCaceres21@Gmail.com>
2026-06-28 22:45:00 -0700

    Add `Collection` field to `queryapi.Request`

--- internal/handler/queryapi/request.go
+++ internal/handler/queryapi/request.go
@@ -18,12 +18,13 @@ func (r Request) last() *Search {
 }
 
 type Search struct {
-	FilterBy string `json:"filter_by"`
-	Page     int    `json:"page"`
-	PerPage  int    `json:"per_page"`
-	Q        string `json:"q"`
-	SelectBy string `json:"query_by"`
-	SortBy   string `json:"sort_by"`
+	Collection string `json:"collection"`
+	FilterBy   string `json:"filter_by"`
+	Page       int    `json:"page"`
+	PerPage    int    `json:"per_page"`
+	Q          string `json:"q"`
+	SelectBy   string `json:"query_by"`
+	SortBy     string `json:"sort_by"`
 }
 
 var SearchSetterError = errors.New("Unable to set Search Filter Field")
